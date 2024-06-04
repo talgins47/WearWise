d1ab0eb3371d6c668e7bd4fbead329c9ec142487
talgins47 <talgins47@gmail.com>
2024-06-04 12:44:24 +0300

    fixing bugs

--- app/src/main/java/com/example/wearwise/MainActivity.java
+++ app/src/main/java/com/example/wearwise/MainActivity.java
@@ -1,21 +1,24 @@
 package com.example.wearwise;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainhome);
-
-    }
-
-    public void onClick(View view) {
-        startActivity(new Intent(MainActivity.this, WorldWeatherFragment.class));
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
